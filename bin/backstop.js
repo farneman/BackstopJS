f22d5c19871d540248ee68d68a97837e79bfb5e8
Josh Farneman <josh@farneman.net>
2015-03-02 21:21:01 -0600

    Added remaining commands to CLI

--- bin/backstop.js
+++ bin/backstop.js
@@ -44,13 +44,53 @@ var configPath = resolve(currentDir, configFile);
  * Commands
  */
 
-program.command('genConfig')
-    .description('Generate a default capture config file')
-    .action(run('geoConfig'));
+program.command('bless')
+    .description('Bless the current capture config')
+    .action(run('bless'));
+
+program.command('clean')
+    .description('Clean the bitmaps_reference directory')
+    .action(run('clean'));
+
+program.command('clean')
+    .description('Clean the bitmaps_reference directory')
+    .action(run('clean'));
 
 program.command('echo')
     .description('Display troubleshooting info')
     .action(run('echo'));
+
+program.command('genConfig')
+    .description('Generate a default capture config file')
+    .action(run('geoConfig'));
+
+program.command('init')
+    .description('Manage dependencies')
+    .action(run('init'));
+
+program.command('openReport')
+    .description('Open the report in a browser')
+    .action(run('openReport'));
+
+program.command('reference')
+    .description('Create a new set of reference bitmaps')
+    .action(run('reference'));
+
+program.command('report')
+    .description('View the report')
+    .action(run('report'));
+
+program.command('start')
+    .description('Start the local web server')
+    .action(run('start'));
+
+program.command('stop')
+    .description('Stop the local web server')
+    .action(run('stop'));
+
+program.command('test')
+    .description("This task will generate a date-named directory with DOM screenshot files as specified in `./capture/config.json` followed by running a report.\n\n NOTE: If there is no bitmaps_reference directory or if the bitmaps_reference directory is empty then a new batch of reference files will be generated in the bitmaps_reference directory.  Reporting will be skipped in this case.")
+    .action(run('test'));
 
 /**
  * Run
